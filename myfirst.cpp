57e024740e4c6c5fa74fed4423a5a61d87c1c029
HyunSuk Lee <blackknight8012@gmail.com>
2017-05-18 23:43:34 +0900

    first commit

---
+++ myfirst.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    cout << "Welcome to c++ world.";
+    cout << endl;
+    cout << "You will not regret it!" << endl;
+
+    return 0;
+}
