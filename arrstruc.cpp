cd0c767e2efa7b1dd30730a1c25e4a3b7484dd0f
HyunSuk Lee <blackknight8012@gmail.com>
2017-05-30 21:23:04 +0900

    remove space

--- arrstruc.cpp
+++ arrstruc.cpp
@@ -16,7 +16,7 @@ int main()
         {"Godzilla", 2000, 565.99},
     };
 
-    cout << "Adding the volums of " 
+    cout << "Adding the volums of "
          << guests[0].name << " and " << guests[1].name << endl
          << guests[0].volume + guests[1].volume
          << " Cubic feet \n";
