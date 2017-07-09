5a465407980536cea456808e2f5717608696b669
HyunSuk Lee <blackknight8012@gmail.com>
2017-07-09 22:06:13 +0900

    add template func(generic) overloading

---
+++ towtemps.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+template <typename T>
+void Swap(T &a, T &b);
+
+template <typename T>
+void Swap(T *a, T *b, int n);
+
+void Show(int const a[]);
+
+const int LIM = 8;
+
+int main()
+{
+    using namespace std;
+    int i = 10;
+    int j = 20;
+    cout << i << ", " << j << endl;
+    Swap(i, j);
+    cout << i << ", " << j << endl;
+
+    int d1[LIM] = {0, 7, 0, 4, 1, 7, 7, 6};
+    int d2[LIM] = {0, 6, 2, 0, 1, 9, 6, 9};
+    Show(d1);
+    Show(d2);
+    Swap(d1, d2, LIM);
+    Show(d1);
+    Show(d2);
+    return 0;
+}
+
+template <typename T>
+void Swap(T &a, T &b) {
+    T temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename T>
+void Swap(T a[], T b[], int n) {
+    T temp;
+    for (int i = 0; i < n; i++) {
+        temp = a[i];
+        a[i] = b[i];
+        b[i] = temp;
+    }
+}
+
+void Show(int const a[]) {
+    using namespace std;
+    cout << a[0] << a[1] << "/";
+    cout << a[2] << a[3] << "/";
+    for (int i = 4; i < LIM; i++) {
+        cout << a[i];
+    }
+    cout << endl;
+}
